cd1bf284fc600bfebcbcc06ea9150fd2d18410b3
XiongWei <your811225@hotmail.com>
2021-09-17 08:05:11 +0800

    转用we UI

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 // app.js
 App({
   // onLaunch() {
-    // // 展示本地存储能力
-    // const logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
+  // // 展示本地存储能力
+  // const logs = wx.getStorageSync('logs') || []
+  // logs.unshift(Date.now())
+  // wx.setStorageSync('logs', logs)
 
-    // // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
+  // // 登录
+  // wx.login({
+  //   success: res => {
+  //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //   }
+  // })
   // },
   globalData: {
+    theme: 'light', // dark
     userInfo: null,
     customLogin: null,
     openid: ""
+  },
+  themeChanged(theme) {
+    this.globalData.theme = theme;
+    themeListeners.forEach((listener) => {
+      listener(theme);
+    });
+  },
+  watchThemeChange(listener) {
+    if (themeListeners.indexOf(listener) < 0) {
+      themeListeners.push(listener);
+    }
+  },
+  unWatchThemeChange(listener) {
+    const index = themeListeners.indexOf(listener);
+    if (index > -1) {
+      themeListeners.splice(index, 1);
+    }
   }
 })
--- pages/partOrder/partOrder.js
+++ pages/partOrder/partOrder.js
@@ -3,9 +3,9 @@ const app = getApp()
 
 import { request } from "../../request/request.js";
 import { addMonth, formatDateByH, formatDate } from "../../utils/util.js"
+import { showToast } from "../../utils/asyncWx.js"
 
 Page({
-
     /**
      * 页面的初始数据
      */
@@ -15,18 +15,44 @@ Page({
         dtSelected: "2021-09-15",
         itemCode: "",
         itemName: "",
-        materiel: null
+        materiel: null,
+        poNum: 0,
+        poRemark: "",
+        savedSuccess: false,
+        savedDialog: false,
+        savedTitle: "",
+        savedBarcode: "",
+        dialogButtons: [
+            {
+                type: 'primary',
+                className: '',
+                text: '复制并关闭',
+                value: 1
+            }
+        ]
     },
 
-    bindDateChange: function (e) {
+    bindDateChange(e) {
         this.setData({
             dtSelected: e.detail.value
         })
     },
 
-    bindItemCodeChange: function (e) {
+    bindItemCodeChange(e) {
         this.setData({
             itemCode: e.detail.value
+        })
+    },
+
+    bindPoNumChange(e) {
+        this.setData({
+            poNum: e.detail.value
+        })
+    },
+
+    bindPoRemarkChange(e) {
+        this.setData({
+            poRemark: e.detail.value
         })
     },
 
@@ -36,16 +62,93 @@ Page({
         }
     },
 
+    openSaveDialog() {
+        this.setData({
+            savedDialog: true
+        });
+    },
+
+    closeSaveDialog() {
+        this.setData({
+            savedDialog: false
+        });
+    },
+
+    closeAndCopy(e) {
+        // console.log(e.detail)
+        // console.log(e.detail.item.value)
+        if (e.detail.item.value == 1) {
+            var endStr = "00000" + this.data.poNum
+
+            wx.setClipboardData({
+                data: "二维码: " + this.data.savedBarcode + "  数量: 00001 - " + endStr.substr(endStr.length - 5),
+                success(res) {
+                }
+            });
+
+            this.setData({
+                savedDialog: false,
+                itemCode: "",
+                itemName: "",
+                poNum: 0,
+                poRemark: ""
+            });
+        }
+    },
+
     async getMaterialInfo() {
         const reData = await request({ url: "Materiel/GetModel/" + this.data.itemCode.trim() });
-        
-        console.log(reData);
-        if (reData != null){
-            this.data.materiel = reData
-            this.data.itemName = reData.prdName
+        // console.log(reData);
+        if (reData != null) {
+            this.setData({
+                materiel: reData,
+                itemName: reData.prdName
+            })
         } else {
-            this.data.materiel = null
-            this.data.itemName = ""
+            this.setData({
+                materiel: null,
+                itemName: ""
+            })
+        }
+    },
+
+    async saveData() {
+        if (this.data.itemName.trim() == "") {
+            showToast({ title: "没有物料信息,请完善" });
+            return
+        }
+        if (this.data.poRemark.trim() == "") {
+            showToast({ title: "请填写备注信息,随便填填也好" });
+            return
+        }
+        if (this.data.poNum <= 0) {
+            showToast({ title: "麻烦把数量填一下" });
+            return
+        }
+
+        const poCusNo = app.globalData.customLogin.cusNo
+        const poPrdNoPart = this.data.itemCode
+        const poNum = this.data.poNum
+        const poEmpId = app.globalData.customLogin.empId
+        const poSelData = this.data.dtSelected
+        const poRemark = this.data.poRemark
+        const saveParams = { poCusNo, poPrdNoPart, poNum, poEmpId, poSelData, poRemark }
+        const { fOK, fMsg } = await request({ url: "PartOrder/Add", method: "POST", data: saveParams });
+
+        if (fOK === "True") {
+            this.setData({
+                savedDialog: true,
+                savedTitle: "保存成功!",
+                savedSuccess: true,
+                savedBarcode: fMsg
+            })
+        } else {
+            this.setData({
+                savedDialog: true,
+                savedTitle: "保存失败，请重试...",
+                savedSuccess: false,
+                savedBarcode: ""
+            })
         }
     },
 
@@ -62,13 +165,14 @@ Page({
             dtStart: formatDateByH(startDate),
             dtEnd: formatDateByH(endDate)
         })
-    },
+    }
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
-
-    }
+    // onShow: function () {
+    // console.log("customLogin:  " + app.globalData.customLogin.cusNo)
+    // console.log("customLogin:  " + app.globalData.customLogin.empId)
+    // }
 
 })
